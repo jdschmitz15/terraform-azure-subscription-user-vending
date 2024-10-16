f1ed369cd50b4a8419e671b22e58f45c4d25db5e
adityakrishnan142 <56053567+adityakrishnan142@users.noreply.github.com>
2024-10-16 16:51:42 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,16 +1,21 @@
 # Output the user information
-output "new_user_principal_name" {
-  description = "The principal name of the newly created user."
+output "new_subscription_id" {
+  description = "ID of the newly created subscription."
+  value       = module.lz_vending.subscription_id
+}
+
+output "tenant_id" {
+  description = "Azure Tenant ID."
+  value       = var.tenant_id
+}
+
+output "new_user_email" {
+  description = "The email ID of the newly created Azure AD user."
   value       = azuread_user.new_user.user_principal_name
 }
 
 output "new_user_password" {
-  description = "The password for the newly created user."
+  description = "The password for the newly created Azure AD user."
   value       = random_password.new_user_password.result
   sensitive   = true
-}
-
-output "login_url" {
-  description = "The login URL for the newly created user."
-  value       = "https://portal.azure.com"
 }
