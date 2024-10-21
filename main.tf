d1f5699d0443b0b751782fc92f36cfe7f7cab172
adityakrishnan142 <56053567+adityakrishnan142@users.noreply.github.com>
2024-10-21 16:14:48 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = "<= 4.5.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
